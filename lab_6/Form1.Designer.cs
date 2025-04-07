8bef135098a7b972f0838e2144e33f59f41120f8
Vlad <vlad.petrovskiy00@mail.ru>
2025-04-07 19:02:03 +0300

    Добавлена подпись к форме

--- lab_6/Form1.Designer.cs
+++ lab_6/Form1.Designer.cs
@@ -39,30 +39,34 @@
             // 
             // sumBox
             // 
-            sumBox.Location = new Point(176, 85);
+            sumBox.Location = new Point(123, 51);
+            sumBox.Margin = new Padding(2, 2, 2, 2);
             sumBox.Name = "sumBox";
-            sumBox.Size = new Size(150, 31);
+            sumBox.Size = new Size(106, 23);
             sumBox.TabIndex = 0;
             // 
             // timeBox
             // 
-            timeBox.Location = new Point(176, 146);
+            timeBox.Location = new Point(123, 88);
+            timeBox.Margin = new Padding(2, 2, 2, 2);
             timeBox.Name = "timeBox";
-            timeBox.Size = new Size(208, 31);
+            timeBox.Size = new Size(147, 23);
             timeBox.TabIndex = 0;
             // 
             // averageBox
             // 
-            averageBox.Location = new Point(176, 205);
+            averageBox.Location = new Point(123, 123);
+            averageBox.Margin = new Padding(2, 2, 2, 2);
             averageBox.Name = "averageBox";
-            averageBox.Size = new Size(150, 31);
+            averageBox.Size = new Size(106, 23);
             averageBox.TabIndex = 0;
             // 
             // calculateRes
             // 
-            calculateRes.Location = new Point(190, 275);
+            calculateRes.Location = new Point(133, 165);
+            calculateRes.Margin = new Padding(2, 2, 2, 2);
             calculateRes.Name = "calculateRes";
-            calculateRes.Size = new Size(112, 34);
+            calculateRes.Size = new Size(78, 20);
             calculateRes.TabIndex = 1;
             calculateRes.Text = "Рассчитать";
             calculateRes.UseVisualStyleBackColor = true;
@@ -71,35 +75,38 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(111, 91);
+            label1.Location = new Point(78, 55);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(67, 25);
+            label1.Size = new Size(45, 15);
             label1.TabIndex = 2;
             label1.Text = "Сумма";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(111, 152);
+            label2.Location = new Point(78, 91);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(64, 25);
+            label2.Size = new Size(42, 15);
             label2.TabIndex = 2;
             label2.Text = "Время";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(9, 208);
+            label3.Location = new Point(6, 125);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(161, 25);
+            label3.Size = new Size(107, 15);
             label3.TabIndex = 2;
             label3.Text = "Среднее значение";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(597, 587);
+            ClientSize = new Size(418, 352);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -107,8 +114,9 @@
             Controls.Add(averageBox);
             Controls.Add(timeBox);
             Controls.Add(sumBox);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Лабораторная работа 6. Многопоточность и  асинхронное программирование. Вариант 10. Ермаков, Петровский";
             ResumeLayout(false);
             PerformLayout();
         }
